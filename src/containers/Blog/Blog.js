f2029cc3e03eaa56c08a21b4e4cfb559ae6ebae0
patricia <patttymq@gmail.com>
2020-11-04 01:39:04 -0400

    add link to no to reload the page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
 import NewPosts from "../../components/NewPost/NewPost";
@@ -16,10 +16,18 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <Link
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </Link>
               </li>
             </ul>
           </nav>
@@ -32,7 +40,6 @@ class Blog extends Component {
         <Route path="/new-post" component={NewPosts} />
 
         {/* prevent to re render the page */}
-        
       </div>
     );
   }
